22c9c3b7877115a2842c3f0cbb6843a347886cbd
Badhon63 <jianrobiul7898@gmail.com>
2026-05-03 12:25:36 +0600

    fixed issues

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 const getAnimals = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/data/animals.json`,
-    {
-      cache: "no-store",
-    },
-  );
+  const res = await fetch("/data/animals.json", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch animals data");
+  }
+
   return res.json();
 };
 
